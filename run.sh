c03452875b8009617eb6ed1b95f54d219e2dc32f
katieannemills <you@example.com>
2026-08-11 22:38:19 -0400

    tag formalism

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
+TAG=${1:?usage: ./run.sh <provenance-tag>   (e.g. ./run.sh OP20260127b)}
 for level in 15_20 15_300 300_700 700_1850 1800_1850
 do
-  sbatch derive.slurm $level
+  sbatch derive.slurm $level "$TAG"
 done
