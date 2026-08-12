42ba4cabaae22369c5337162eee9806c1d91e6bb
Katie Mills <you@example.com>
2026-08-12 09:23:30 -0600

    run config rc for prod_260810-gcos

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-for level in 15_20 15_300 300_700 700_1850 1800_1850
+for level in 15_20 15_300 300_700 700_1000 700_1850 1800_1850
 do
   outputdir=/scratch/alpine/wimi7695/ohc_prod/results
   sbatch derive.slurm ${outputdir}/OHC_*${level}*.nc $outputdir prod_260810-gcos
