5bca6790f6f2c3051523811e35623ce1cece36a3
Katie Mills <you@example.com>
2026-08-11 20:44:15 -0600

    run config

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 for level in 15_20 15_300 300_700 700_1850 1800_1850
 do
-  sbatch derive.slurm $level
+  outputdir=/scratch/alpine/wimi7695/ohc_prod/results
+  sbatch derive.slurm ${outputdir}/OHC_*${level}*.nc $outputdir
 done
